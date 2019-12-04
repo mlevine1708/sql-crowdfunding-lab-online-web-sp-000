9fba8f5ba7b05565b54c1dfba2e67dc29cc9f41e
Micah Levine <micah.levine@outlook.com>
2019-12-04 00:49:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,4 +9,23 @@ INSERT INTO projects VALUES (8, "Elsa", "Apps", 3300, 05/30/19, 07/30/19);
 INSERT INTO projects VALUES (9, "Luke", "Apps", 3300, 05/30/19, 07/30/19);
 INSERT INTO projects VALUES (10, "David", "Apps", 3300, 05/30/19, 07/30/19);
 
-INSERT INTO users VALUES ()
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
+INSERT INTO users VALUES (1, "Micah", 45);
